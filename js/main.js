93dc37e19d5c86122ee7e5d0122cdcb94e64e6f7
Wes Cravens <wcravens@cortex-it.com>
2015-09-17 08:00:33 -0500

    Position feature content just over the hero unit on the bottom

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(window).scroll(function() {
-    if($(this).scrollTop() > 300) {
+    if($(this).scrollTop() > 200) {
         $('body#landing-page a.navbar-brand').addClass('navbar-brand-opaque');
     } else {
         $('body#landing-page a.navbar-brand').removeClass('navbar-brand-opaque');
