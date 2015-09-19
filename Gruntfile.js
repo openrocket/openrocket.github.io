0183f68a9358cde3befe012da5b438296703b0ed
Wes Cravens <wcravens@cortex-it.com>
2015-09-18 20:49:06 -0500

    Fix gruntfile to remove watch loops, builds triggering builds

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function( grunt ) {
         options: { livereload: true },
       },
       jekyll: {
-        files: [ '**/*.html', '**/*.js', '**/*.png', '**/*.svg', '_config.yml' ],
+        files: [ '*.html', '_includes', '_layouts', 'css/**', 'js/**', 'img/**', '_config.yml' ],
         tasks: [ 'jekyll' ]
       }
     },
