54c67a21cf9119aa9006ef69f3e936f600607289
Wes Cravens <wcravens@cortex-it.com>
2015-09-21 06:55:34 -0500

    A-B test on navbar; staging feature copy on landing page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-$(window).scroll(function() {
-    if($(this).scrollTop() > 200) {
-        $('body#landing-page a.navbar-brand, body#landing-page #navbar_download_button').addClass('navbar-visible-on-scroll');
-    } else {
-        $('body#landing-page a.navbar-brand, body#landing-page #navbar_download_button').removeClass('navbar-visible-on-scroll');
-    }
+$(document).ready(function(){
+
+  $(window).scroll(function() {
+      if($(this).scrollTop() > 200) {
+          $('body#landing-page a.navbar-brand, body#landing-page #navbar_download_button').addClass('navbar-visible-on-scroll');
+      } else {
+          $('body#landing-page a.navbar-brand, body#landing-page #navbar_download_button').removeClass('navbar-visible-on-scroll');
+      }
+  });
+
+  $('body#landing-page nav.navbar').addClass('navbar-fixed-top');
+
 });
+
