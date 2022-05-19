3af899584807e9d3fec4b90f26378e0ad6046db8
SiboVG <sibo.vangool@hotmail.com>
2022-05-19 20:42:17 +0200

    Fix JAR download option not showing up

--- downloads/downloads_config.js
+++ downloads/downloads_config.js
@@ -2,12 +2,12 @@ const downloads_config = [
     {
         name: "22.02.beta.03",
         macOSFile: "OpenRocket-22.02.beta.03-macOS.dmg",
-        macOSInstr: "downloads/instructions/macOS_22.02.beta.02.md",
+        macOSInstr: "downloads/instructions/macOS_22.02.beta.03.md",
         LinuxFile: "OpenRocket-22.02.beta.03-Linux.sh",
         LinuxInstr: "downloads/instructions/Linux_22.02.beta.03.md",
         WindowsFile: "OpenRocket-22.02.beta.03-Windows.exe",
         WindowsInstr: "downloads/instructions/Windows_22.02.beta.03.md",
-        JAR: "OpenRocket-22.02.beta.03.jar",
+        JARFile: "OpenRocket-22.02.beta.03.jar",
         JARInstr: "downloads/instructions/JAR_22.02.beta.03.md"
     },
     {
@@ -18,7 +18,7 @@ const downloads_config = [
         LinuxInstr: "downloads/instructions/Linux_15.03.md",
         WindowsFile: "OpenRocket-15.03-installer.exe",
         WindowsInstr: "downloads/instructions/Windows_15.03.md",
-        JAR: "OpenRocket-15.03.jar",
+        JARFile: "OpenRocket-15.03.jar",
         JARInstr: "downloads/instructions/JAR_15.03.md"
     }
 ]
